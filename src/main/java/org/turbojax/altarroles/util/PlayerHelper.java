34929dbf0ac2e67898cdb13e022d3a0a6c37fe53
TurboJax <turbojax07@gmail.com>
2026-07-16 19:04:05 -0400

    Adding the lock tag to PlayerHelper

--- src/main/java/org/turbojax/altarroles/util/PlayerHelper.java
+++ src/main/java/org/turbojax/altarroles/util/PlayerHelper.java
@@ -9,6 +9,7 @@ import org.turbojax.altarroles.Role;
 public class PlayerHelper {
     public static final NamespacedKey ROLE_KEY = new NamespacedKey("altar_roles", "role");
     public static final NamespacedKey HIDDEN_ROT_KEY = new NamespacedKey("altar_roles", "hidden_rot");
+    public static final NamespacedKey ROLE_LOCK_KEY = new NamespacedKey("altar_roles", "lock");
 
     public static Role getRole(OfflinePlayer player) {
         return player.getPersistentDataContainer().getOrDefault(ROLE_KEY, CustomPDCTypes.ROLE, Role.TEMP_HUMAN);
@@ -29,5 +30,17 @@ public class PlayerHelper {
         }
 
         player.getPersistentDataContainer().set(HIDDEN_ROT_KEY, PersistentDataType.BOOLEAN, true);
+    }
+
+    public static void lock(Player player) {
+        player.getPersistentDataContainer().set(ROLE_LOCK_KEY, PersistentDataType.BOOLEAN, true);
+    }
+
+    public static boolean isLocked(OfflinePlayer player) {
+        return player.getPersistentDataContainer().has(ROLE_LOCK_KEY);
+    }
+
+    public static void unlock(Player player) {
+        player.getPersistentDataContainer().remove(ROLE_LOCK_KEY);
     }
 }
