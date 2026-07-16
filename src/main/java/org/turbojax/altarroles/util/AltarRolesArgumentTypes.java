6222e0967eca69b42ba9e76465094900a0177e92
TurboJax <turbojax07@gmail.com>
2026-07-16 14:10:40 -0400

    Adding a class to hold instances of the custom argument types

---
+++ src/main/java/org/turbojax/altarroles/util/AltarRolesArgumentTypes.java
@@ -0,0 +1,6 @@
+package org.turbojax.altarroles.util;
+
+public class AltarRolesArgumentTypes {
+    public static final RoleArgumentType ROLE = new RoleArgumentType();
+    public static final TeamArgumentType TEAM = new TeamArgumentType();
+}
