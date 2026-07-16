1eafc2972c49f9f2d04a622dee0b8a3a45f67681
TurboJax <turbojax07@gmail.com>
2026-07-16 12:09:30 -0400

    Creating a slf4j logger

--- src/main/java/org/turbojax/altarroles/AltarRoles.java
+++ src/main/java/org/turbojax/altarroles/AltarRoles.java
@@ -2,9 +2,13 @@ package org.turbojax.altarroles;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.turbojax.altarroles.listeners.PlayerDeathListener;
 
 public final class AltarRoles extends JavaPlugin {
+    public static final Logger LOGGER = LoggerFactory.getLogger("AltarRoles");
+
     @Override
     public void onEnable() {
         // Plugin startup logic
