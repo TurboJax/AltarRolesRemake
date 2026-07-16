05467ea8be2a5edb6db8ee5602a92917b2350306
TurboJax <turbojax07@gmail.com>
2026-07-16 18:51:15 -0400

    Having TeamArgumentType return a Role instead of a String

--- src/main/java/org/turbojax/altarroles/util/TeamArgumentType.java
+++ src/main/java/org/turbojax/altarroles/util/TeamArgumentType.java
@@ -3,27 +3,30 @@ package org.turbojax.altarroles.util;
 import com.mojang.brigadier.LiteralMessage;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
+import org.turbojax.altarroles.Role;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public class TeamArgumentType implements ArgumentType<String> {
+public class TeamArgumentType implements CustomArgumentType<Role,String> {
     private static final List<String> EXAMPLES = List.of("human", "vampire", "pale");
     private static final DynamicCommandExceptionType PARSE_ERROR = new DynamicCommandExceptionType(team -> new LiteralMessage("Invalid team \"" + team + "\""));
 
     @Override
-    public String parse(StringReader reader) throws CommandSyntaxException {
+    public Role parse(StringReader reader) throws CommandSyntaxException {
         String team = reader.readUnquotedString();
 
-        if (team.equalsIgnoreCase("human")) return "human";
-        if (team.equalsIgnoreCase("vampire")) return "vampire";
-        if (team.equalsIgnoreCase("pale")) return "pale";
+        if (team.equalsIgnoreCase("human")) return Role.TEMP_HUMAN;
+        if (team.equalsIgnoreCase("vampire")) return Role.TEMP_VAMPIRE;
+        if (team.equalsIgnoreCase("pale")) return Role.TEMP_PALE;
 
         throw PARSE_ERROR.create(team);
     }
@@ -40,5 +43,10 @@ public class TeamArgumentType implements ArgumentType<String> {
     @Override
     public Collection<String> getExamples() {
         return EXAMPLES;
+    }
+
+    @Override
+    public ArgumentType<String> getNativeType() {
+        return StringArgumentType.word();
     }
 }
