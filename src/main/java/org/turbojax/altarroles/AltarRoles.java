37eaa1c6d2fb1b8f4b074b8cfc1443b53756760e
TurboJax <turbojax07@gmail.com>
2026-07-16 12:08:22 -0400

    Registering the PlayerDeathListener

--- src/main/java/org/turbojax/altarroles/AltarRoles.java
+++ src/main/java/org/turbojax/altarroles/AltarRoles.java
@@ -1,12 +1,14 @@
 package org.turbojax.altarroles;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.turbojax.altarroles.listeners.PlayerDeathListener;
 
 public final class AltarRoles extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(new PlayerDeathListener(), this);
     }
 
     @Override
