5302a52fd6ed1c5607ed86b7e91f1a5d8a5876d3
TurboJax <turbojax07@gmail.com>
2026-07-16 18:09:54 -0400

    Adding the config parser and my first config

---
+++ src/main/java/org/turbojax/altarroles/MainConfig.java
@@ -0,0 +1,95 @@
+package org.turbojax.altarroles;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainConfig {
+    public static final AltarRoles plugin = JavaPlugin.getPlugin(AltarRoles.class);
+    public static final File file = new File(plugin.getDataFolder(), "config.yml");
+    public static final FileConfiguration config = new YamlConfiguration();
+
+    private static final MainConfig instance = new MainConfig();
+
+    private MainConfig() {}
+
+    public static MainConfig getInstance() {
+        return instance;
+    }
+
+    /**
+     * Reloads the configuration.
+     *
+     * @return Whether the configuration was loaded successfully.
+     */
+    public boolean load() {
+        // Not doing anything if the plugin isn't enabled
+        if (!plugin.isEnabled()) {
+            AltarRoles.LOGGER.error("{} not loaded, cannot load {}.", plugin.getName(), file.getName());
+            return false;
+        }
+
+        // Creating the file if it doesn't exist.
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            plugin.saveResource(file.getName(), true);
+        }
+
+        // Loading the config
+        try {
+            config.load(file);
+            AltarRoles.LOGGER.info("Successfully loaded {}", file.getName());
+            return true;
+        } catch (InvalidConfigurationException e) {
+            AltarRoles.LOGGER.warn("{} contains an invalid YAML configuration.  Verify the contents of the file.", file.getName());
+        } catch (IOException e) {
+            AltarRoles.LOGGER.error("Could not find {}.  Check that it exists.", file.getName());
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    /**
+     * Writes the config to the file.
+     * 
+     * @return Whether or not the config was successfully written.
+     */
+    public boolean save() {
+        // Not doing anything if the plugin isn't enabled
+        if (!plugin.isEnabled()) {
+            AltarRoles.LOGGER.error("{} not loaded, cannot save {}.", plugin.getName(), file.getName());
+            return false;
+        }
+
+        // Creating the file if it doesn't exist.
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                return false;
+            }
+        }
+
+        // Saving the config
+        try {
+            config.save(file);
+            AltarRoles.LOGGER.info("Saved {}", file.getName());
+            return true;
+        } catch (IOException e) {
+            AltarRoles.LOGGER.warn("Could not save {}.  Make sure the user has write permissions.", file.getName());
+        }
+
+        return false;
+    }
+
+    public String altarPlugin() {
+        return config.getString("altar_plugin", "official").toLowerCase();
+    }
+
+    public void applyUpdates() {}
+}
