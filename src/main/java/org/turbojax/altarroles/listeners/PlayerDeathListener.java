76d6144712661c2749295522a8c1c9f5c5745a46
TurboJax <turbojax07@gmail.com>
2026-07-16 12:08:12 -0400

    Handling role changes

--- src/main/java/org/turbojax/altarroles/listeners/PlayerDeathListener.java
+++ src/main/java/org/turbojax/altarroles/listeners/PlayerDeathListener.java
@@ -1,5 +1,32 @@
 package org.turbojax.altarroles.listeners;
 
-public class PlayerDeathListener {
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.turbojax.altarroles.Role;
+import org.turbojax.altarroles.util.PlayerHelper;
 
+public class PlayerDeathListener implements Listener {
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        Player dead = event.getPlayer();
+        Player killer = dead.getKiller();
+
+        Role deadRole = PlayerHelper.getRole(dead);
+        Role killerRole = PlayerHelper.getRole(killer);
+
+        // TODO: Handle legendaries
+        // Should legendary kills convert the player to the wielder's team or the weapon's team?
+        // Probably the wielder's team.
+
+        // True roles cannot be converted by normal kills
+        if (!deadRole.isTrue()) return;
+
+        // Humans cannot convert people back to human
+        if (killerRole.isHuman()) return;
+
+        // Converting the dead player to the killer's team
+        PlayerHelper.setRole(dead, killerRole.toTemp());
+    }
 }
