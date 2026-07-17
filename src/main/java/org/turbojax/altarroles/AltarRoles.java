eb954d5cadae97c41b7d0b81bb43eff320f137f7
TurboJax <turbojax07@gmail.com>
2026-07-16 22:16:59 -0400

    Saving the default config and registering commands

--- src/main/java/org/turbojax/altarroles/AltarRoles.java
+++ src/main/java/org/turbojax/altarroles/AltarRoles.java
@@ -1,9 +1,11 @@
 package org.turbojax.altarroles;
 
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.turbojax.altarroles.commands.AltarRolesCommand;
 import org.turbojax.altarroles.listeners.PlayerDeathListener;
 import org.turbojax.altarroles.listeners.PlayerJoinListener;
 
@@ -12,7 +14,16 @@ public final class AltarRoles extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Saving the default config
+        MainConfig.save();
+        MainConfig.load();
+
+        // Registering commands
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+            commands.registrar().register(new AltarRolesCommand().build("altarroles"));
+        });
+
+        // Registering event handlers
         Bukkit.getPluginManager().registerEvents(new PlayerDeathListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
     }
